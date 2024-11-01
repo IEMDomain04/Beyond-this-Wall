de553790b5e58b92c691ac3582cd7126db60df58
IEMDomain04.dc <emmanman0011@gmail.com>
2024-11-01 15:02:49 +0800

    Added animation aos

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,11 +4,11 @@ import './NavBar.css';
 function NavBar() {
     return (
       <nav className="navbar">
-        <div className="branding">
+        <div className="branding" data-aos="fade-right" data-aos-duration="3000">
             <img src="https://cdn-icons-png.flaticon.com/128/9920/9920949.png" alt="Brand Logo" />
             <span className="title">Beyond the Walls</span>
         </div>
-        <div className="nav-links">
+        <div className="nav-links" data-aos="fade-right" data-aos-duration="3000">
             <a href="#about">About</a>
             <a href="#places">Places</a>
         </div>
