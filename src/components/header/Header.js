92bc462211f9146c1e9cfac90ac5f03f56a869b5
IEMDomain04.dc <emmanman0011@gmail.com>
2024-11-26 15:37:48 +0800

    Header Addded. TODO: Add more contents

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,14 +3,22 @@ import './Header.css';
 
 function Header() {
   return (
-    <nav className="header">
+    <header className="header">
+
       <div id="header-texts">
-        <h1 id="title"> Welcome to Beyond the Walls!</h1>
-        <p id="subtitle"> This is a website built for just intramuros attractions and places you want to see!</p>
-        <a href=""> <button id="action-btn"> See Places! </button> </a>
+        <h1 id="title">Welcome to Beyond the Walls!</h1>
+        <p id="subtitle">
+          This is a website built for just Intramuros attractions and places you want to see!
+        </p>
+        <a href="#places"> 
+          <button id="action-btn">See Places!</button>
+        </a>
       </div>
-    </nav >
-    );
+
+      <img id="intra-img" src="../../../assets/header-img.svg" alt="Illustration of Intramuros attractions" />
+
+    </header>
+  );
 }
 
 export default Header;
